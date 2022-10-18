28c46cc42ff46f25ee60fd438f1ce6ee8d5f7ea0
shraddhabaheti <shraddhabeheti123@gmail.com>
2022-10-18 15:54:29 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Student from './Student';
+import Hello from './Hello';
+import Login from './Login';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
        <h3>hello I am shraddha baheti</h3>
     
       <Student/>
+      <Hello/>
+      <Login/>
     </div>
   );
 }
